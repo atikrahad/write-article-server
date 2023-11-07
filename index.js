33f449739ca4477e76ea85375d0654b6c4b89401
unknown <atikrahad1@gmail.com>
2023-11-07 07:41:53 +0600

    create a database and post methode setup

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1lk0tsy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1lk0tsy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +23,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const blogpostcollection = client.db("blogpostDB").collection("blogpost");
+
+    app.post("/blogpost", async (req, res) => {
+      const post = req.body;
+      const result = await blogpostcollection.insertOne(post);
+      res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(

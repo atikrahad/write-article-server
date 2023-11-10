d1dda28581e6da85d69a6c93e14c43ecd9037014
unknown <atikrahad1@gmail.com>
2023-11-10 22:46:33 +0600

    fixing all bug

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-var cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://blog-site-68835.web.app"],
     credentials: true,
   })
 );
@@ -28,30 +28,47 @@ const client = new MongoClient(uri, {
   },
 });
 
+const tokenverify = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
+  jwt.verify(token, process.env.RANDOM_SECRET_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorize" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
 
     const blogpostcollection = client.db("blogpostDB").collection("blogpost");
     const commentCollection = client.db("commentsDB").collection("comments");
     const wiahListCollection = client.db("wishlistDB").collection("wishlist");
 
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      const result = await jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
+    app.post("/jwt", (req, res) => {
+      const userdata = req.body;
+      const token = jwt.sign(userdata, process.env.RANDOM_SECRET_TOKEN, {
         expiresIn: "1h",
       });
       res
-        .cookie("token", result, {
+        .cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === 'production', 
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         })
         .send({ success: true });
     });
 
-    app.get("/comment", async (req, res) => {
+    app.get("/comment",tokenverify, async (req, res) => {
       const id = req.query.id;
+      console.log(req.cookies);
       const filterId = { blogId: id };
       const result = await commentCollection.find(filterId).toArray();
       res.send(result);
@@ -68,21 +85,19 @@ async function run() {
       res.send({ count });
     });
 
-    app.get("/recentpost",async (req, res) => {
+    app.get("/recentpost", async (req, res) => {
       const quairy = [
-        
         {
-          $sort: { currentdate: -1 }, 
+          $sort: { currentdate: -1 },
         },
         {
-          $limit: 6, 
+          $limit: 6,
         },
       ];
 
-      const result = await blogpostcollection.aggregate(quairy).toArray()
+      const result = await blogpostcollection.aggregate(quairy).toArray();
 
-      res.send(result)
-
+      res.send(result);
     });
 
     app.get("/allpost", async (req, res) => {
@@ -140,7 +155,8 @@ async function run() {
     });
 
     app.get("/wishlist", async (req, res) => {
-      const result = await wiahListCollection.find().toArray();
+      const quairy = req.query.email
+      const result = await wiahListCollection.find({email: quairy}).toArray();
       res.send(result);
     });
 
@@ -186,10 +202,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
